a6a7c3eb734bc500795aac9fdb4521f5d29df869
Yashwanth <yashwanth.v357edu@gmail.com>
2026-06-18 20:24:55 +0530

    feat: add Navbar component and integrate it into Layout; enhance Dashboard with resume creation and upload features

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,8 +1,15 @@
- 
- const Layout = () => {
-   return (
-     <div>Layout</div>
-   )
- }
- 
- export default Layout
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+const Layout = () => {
+  return (
+    <div>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
