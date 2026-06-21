ebb10ea0aba46cd7e807cd0c586926be3d725112
Yashwanth <yashwanth.v357edu@gmail.com>
2026-06-21 13:43:51 +0530

    feat: add user authentication middleware and user routes for registration, login, and data retrieval

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import connectDB from "./src/config/db.js";
-
+import userRouter from "./src/routes/userRoutes.js";
 
 dotenv.config();
 
 const app = express();
 
-await connectDB()
+await connectDB();
 
 app.use(express.json());
+app.use(cors());
+
+app.use('/api/users',userRouter)
 
 const PORT = process.env.PORT || 5000;
 
